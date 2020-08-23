513657ac4c240bc35ddce8e6ec68c140d6bb4206
levan-dolidze <64006772+levan-dolidze@users.noreply.github.com>
2020-08-23 14:58:20 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,201 @@
+// const array =[
+// {
+//     name:'levan',
+//     age:20
+// },
+// {
+//     name:'sofo',
+//     age:27
+// },
+// {
+//     name:'mari',
+//     age:21
+// }
+
+// ];
+
+// const arrayOfAges=array.map((element)=>{
+// return element.age;
+// });
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+// const newObj={
+//     name:"quertu",
+//     age:22
+// }
+// const newObj1={
+//     name:"okk",
+//     age:22
+// }
+
+// array.splice(0,2,newObj,newObj1)
+// console.log(array)
+
+
+
+
+// array.forEach((element) =>{
+//     // console.log(element)
+// })
+
+
+// const hasUserAbove25 = array.some((element)=>{
+// return element.age>=25;
+// });
+// console.log(hasUserAbove25)
+
+// const everyUserAbove=array.every((element)=>{
+// return element.age>=25
+// });
+// console.log(everyUserAbove)
+
+// array.splice(0,1)
+
+
+ 
+// const arr=(arr1=[1,2,3])
+
+
+
+
+
+// const logAge =(age=10,name="leva")=>{
+//     const welcomeMessage=`hello ${name} you are ${age} years old`
+    
+//     console.log(welcomeMessage);
+
+// }
+// logAge(20);
+
+//task
+// const consolid=[];
+// const array1= [1,2,3];
+// const array2=[10,20,30];
+// for (let i = 0; i < array1.length; i++) {
+//     consolid.push(array1[i])
+   
+// }
+
+// for (let k = 0; k < array2.length; k++) {
+//     consolid.push(array2[k])
+    
+// }
+// console.log("consolidation: ", consolid);
+
+
+   const array=[
+    {
+name:"levan",
+age:22
+
+    },
+    {
+        name:"sofo",
+        age:26
+        
+    },
+    {
+         name:"ani",
+        age:21
+                
+                
+     }
+   ];
+
+console.log(array)
+
+const qwerty={
+    name:"ytrewq",
+    age:22
+}
+const qwert={
+    name:"ytrewq",
+    age:22
+}
+// vshlit pirveli indeqsdan mokolebuli 2 elements masividan
+
+// array.splice(0,1)
+array.splice(1,2,qwerty,qwert)
+console.log(array);
+
+
+
+
+
+
+
+
+
+
+//    const arrayOfAge=array.map((element)=>{
+// return element.name;
+//    });
+
+//    console.log(arrayOfAge)
+
+
+//    array.forEach((element)=>{
+
+//   console.log(element);
+//    })
+
+//    const as=[1,2,3,4,5,6]
+
+//    as.forEach((element)=>{
+
+//     console.log(element)
+//    })
+
+
+//  const SOME1=array.some((element)=>{
+// return element.age>=25;
+
+// });
+// console.log(SOME1)
+
+
+// const sum=[];
+// const arr = [1,2,3,4,5];
+// const arr1=[10,20,30]
+// for (let i = 0; i < arr.length; i++) {
+    
+// sum.push(arr[i])
+// }
+// for (let k = 0; k< arr1.length; k++) {
+//     sum.push(arr1[k])
+    
+    
+// }
+// console.log(sum)
+
+
+
+// const sum1=[];
+// const arr3=[1,2,3,4,5,6]
+// const arr4=[10,20,30,45]
+
+//    arr3.forEach((element)=>{
+//        sum1.push(arr3[element])
+
+//    })
+//    arr4.forEach((element)=>{
+//     sum1.push(arr4[element])
+
+// })
+// console.log(sum1)
+
+
+
+// const everyUserAbove25=array.every((element)=>{
+
+//     return element.age>=25;
+// })
+// console.log(everyUserAbove25)
+
+
+// array.splice(0,1)
+// console.log(array)
